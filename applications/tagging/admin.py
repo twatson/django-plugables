73e74d3ab4b9dd8770a90f9cd255fc38420da0e8
Bryan Veloso <bryan@revyver.com>
2008-06-21 18:32:05 -0700

    Removing the tagging pluggable so it can be replaced.

--- applications/tagging/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from tagging.models import Tag, TaggedItem
-
-
-admin.site.register(Tag)
-admin.site.register(TaggedItem)
--- applications/tagging/register.py
+++
@@ -1,6 +0,0 @@
-# Administration
-from tagging import admin
-
-# Template Tags
-from django import template
-template.add_to_builtins('tagging.templatetags.tagging_tags')
